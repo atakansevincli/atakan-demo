5b4fc675dce09f14eb44a09109b6f1cee46a7501
atakansevincli <atakan.sevincli@hotmail.com>
2024-07-02 03:30:26 +0300

    second commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/json", (req, res) => {
+app.get("/", (req, res) => {
   const message = req.query.message || "Hello, World!";
   res.json({ message });
 });
